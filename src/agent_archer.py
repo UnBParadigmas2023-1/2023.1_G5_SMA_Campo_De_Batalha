b336c67c8c5e6192b327989f3b2a8f96c514ecd5
kayrocesar <kayro.cesar.kc@gmail.com>
2023-07-01 20:56:26 -0300

    logica de operação do arqueiro adicionada

--- src/agent_archer.py
+++ src/agent_archer.py
@@ -11,4 +11,26 @@ class AgenteArcher(mesa.Agent):
         self.vida = 5
         self.range = 3
 
-   
+    def step(self):
+        self.operate()
+
+    def operate(self) -> None:
+        if self.vida <= 0:
+            self.model.grid.remove_agent(self)
+            self.model.schedule.remove(self)
+            return
+            
+        attack = False
+        for i in range(self.range):
+            print(self.unique_id, self.pos, i+1)
+            for vizinho in self.model.grid.iter_neighbors(  self.pos, moore=True, radius=i + 1):
+                print(vizinho.unique_id, "vizinho de", self.unique_id, "em", self.pos)
+                if vizinho.tipo != self.tipo:
+                    print(vizinho.unique_id, "atacado por", self.unique_id)
+                    vizinho.vida -= 1
+                    attack = True
+
+                    if self.vida <= 0 and self.pos is not None:
+                        self.model.grid.remove_agent(self)
+                        self.model.schedule.remove(self)
+                        return
