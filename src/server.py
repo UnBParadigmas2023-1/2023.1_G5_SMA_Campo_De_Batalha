4772b0ec74adcc6cdac0762afadcba9fa8bd5428
kayrocesar <kayro.cesar.kc@gmail.com>
2023-07-01 21:01:16 -0300

    slider da pagina adicionado
    Co-Authored-By: Victor Hugo <50213514+8ifq3@users.noreply.github.com>
    Co-Authored-By: Flavio Vieira <31935777+flaviovl@users.noreply.github.com>
    Co-Authored-By: Lucas Lopes  <38164895+lucaslop@users.noreply.github.com>

--- src/server.py
+++ src/server.py
@@ -1,4 +1,25 @@
+import mesa
 from mesa.visualization.ModularVisualization import ModularServer
+
+
+model_params = {
+        "num_aliados": mesa.visualization.Slider(
+            name="Número de agentes do time azul",
+            min_value=0,
+            max_value=40,
+            step=1,
+            value=3,
+        ),
+        "num_inimigos": mesa.visualization.Slider(
+            name="Número de agentes do time vermelho",
+            min_value=0,
+            max_value=40,
+            step=1,
+            value=3,
+        ),
+        "width": 10,
+        "height": 10,
+    }
 
 server = ModularServer(
     Modelo,
