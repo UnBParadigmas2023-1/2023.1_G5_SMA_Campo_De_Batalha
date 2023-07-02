8d826f51ab99fff6651a95d43fbdf991d3dd3544
Thalisson-Alves <thalisson2030@gmail.com>
2023-07-02 19:59:09 -0300

    fix attack concurrency
    Co-authored-by: Mateus Maia <mateusmaiamaia@hotmail.com>

--- src/agent_lancer.py
+++ src/agent_lancer.py
@@ -15,22 +15,19 @@ class AgenteLancer(mesa.Agent):
         self.operate()
 
     def operate(self) -> None:
-        if self.vida <= 0:
-                self.model.grid.remove_agent(self)
-                self.model.schedule.remove(self)
-                return
         for vizinho in self.model.grid.iter_neighbors( self.pos, moore=True, radius=self.range):
                 print(vizinho)
                 print(vizinho.unique_id, "vizinho de", self.unique_id, "em", self.pos)
                 if vizinho.tipo != self.tipo:
                     print(vizinho.unique_id, "atacado por", self.unique_id)
                     vizinho.vida -= 1
-                    if self.vida <= 0 and self.pos is not None:
-                        self.model.grid.remove_agent(self)
-                        self.model.schedule.remove(self)
-                        return
 
     def advance(self) -> None:
+        if self.vida <= 0:
+            self.model.grid.remove_agent(self)
+            self.model.schedule.remove(self)
+            return
+
         if self.pos != None:
             posicoes_ocupadas = [agente.pos for agente in self.model.schedule.agents]
             pos = posicaoVazia(self.model, posicoes_ocupadas)
