0c851c3b806322a8bf1d49e73165bda1276e56d0
kayrocesar <kayro.cesar.kc@gmail.com>
2023-07-01 21:07:01 -0300

    add total de agentes de cada tipo e modelo de cada agente
    Co-Authored-By: Victor Hugo <50213514+8ifq3@users.noreply.github.com>
    Co-Authored-By: Flavio Vieira <31935777+flaviovl@users.noreply.github.com>
    Co-Authored-By: Lucas Lopes  <38164895+lucaslop@users.noreply.github.com>

--- src/server.py
+++ src/server.py
@@ -19,7 +19,58 @@ model_params = {
         ),
         "width": 10,
         "height": 10,
+}
+
+def qtdAliadosInimigos(model):
+    aliados = [r for r in model.schedule.agents if "aliado" in r.tipo]
+    inimigos = [p for p in model.schedule.agents if "inimigo" in p.tipo]
+    return f"Time azul: {len(aliados)}<br>Time vermelho: {len(inimigos)}"
+
+
+def modelo_desenho(agent):
+    if agent is None:
+        return
+
+    portrayal = {
+        "Filled": "true",
+        "Layer": 0,
+        "text_color": "White",
+        "text": f"{agent.vida}-{agent.unique_id}",  # Exibe o nível de vida do agente
     }
+        # circulo = lanceiro
+        # cavaleiro = quadrado
+        # arqueiro = retangulo
+    if type(agent) is AgenteArcher:
+        # portrayal["Shape"] = "./assets/archer.png"
+        # portrayal["Shape"] = "arrowHead"
+        # portrayal["scale"] = 0.8
+        # portrayal["heading_x"] = 0.8
+        # portrayal["heading_y"] = 0.8
+        portrayal["Shape"] = "rect"
+        portrayal["w"] = 0.99
+        portrayal["h"] = 0.4
+
+    elif type(agent) is AgenteKnight:
+
+        portrayal["Shape"] = "rect"
+        portrayal["w"] = 0.8
+        portrayal["h"] = 0.8
+
+    else:
+        portrayal["Shape"] = "circle"
+        portrayal["r"] = 0.8
+
+    if "aliado" in agent.tipo:
+        
+        portrayal["Color"] = "#0000FF"  # Cor azul para agentes aliados
+
+    else:
+        
+        portrayal["Color"] = "#8B0000"  # Cor azul para agentes aliados
+
+  
+
+    return portrayal
 
 server = ModularServer(
     Modelo,
