d06449b438ee623d9823fee8661bafcb4be73c24
Flavio Vieira Leao <flavio.vl@gmail.com>
2023-07-01 21:15:44 -0300

    Adicionado logica de ataque e avancar posicao
    Co-authored-by: Thalisson-Alves <thalisson2030@gmail.com>

--- src/agent_knight.py
+++ src/agent_knight.py
@@ -12,4 +12,31 @@ class AgenteKnight(mesa.Agent):
         self.range = 2
 
     def step(self):
-        pass
+        self.operate()
+
+    def operate(self) -> None:
+        if self.vida <= 0:
+            self.model.grid.remove_agent(self)
+            self.model.schedule.remove(self)
+            return
+
+        attack = False
+        for i in range(self.range):
+            print(self.unique_id, self.pos, i + 1)
+            for vizinho in self.model.grid.iter_neighbors(self.pos, moore=True, radius=i + 1):
+                print(vizinho.unique_id, "vizinho de", self.unique_id, "em", self.pos)
+                if vizinho.tipo != self.tipo:
+                    print(vizinho.unique_id, "atacado por", self.unique_id)
+                    vizinho.vida -= 1
+                    attack = True
+
+                    if self.vida <= 0 and self.pos is not None:
+                        self.model.grid.remove_agent(self)
+                        self.model.schedule.remove(self)
+                        return
+
+    def advance(self) -> None:
+        if self.pos != None:
+            posicoes_ocupadas = [agente.pos for agente in self.model.schedule.agents]
+            pos = posicaoVazia(self.model, posicoes_ocupadas)
+            self.model.grid.move_agent(self, pos)
